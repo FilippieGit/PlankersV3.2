07338ff7f581bd60cc62682f8864a5305a768eb3
Mogitures <davibeserker@outlook.com.br>
2024-09-12 20:28:58 -0300

    oi

--- app/src/main/java/br/com/etecia/myapp2/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp2/LoginActivity.java
@@ -1,6 +1,9 @@
 package br.com.etecia.myapp2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //Declarar as variáveis globais:
+    Button btnEsqSenha, btnCriarconta, btnEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,37 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Apresentando as variáveis do java para o xml
+
+        btnEsqSenha = findViewById(R.id.btnEsqSenha);
+        btnCriarconta = findViewById(R.id.btnCriarconta);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        //Criando as ações dos botões
+
+        btnEsqSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RecuperarSenhaActivity.class));
+                finish();
+
+            }
+        });
+
+        btnCriarconta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CadastrarSenhaActivity.class));
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+            }
+        });
+
     }
 }
